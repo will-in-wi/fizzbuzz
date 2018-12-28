11292e47850d412530da9adf73a7dc328d0e5ea7
William Johnston <william@johnstonhaus.us>
2018-12-28 08:47:53 -0600

    Continuing to play with assembler

--- asm_x86_macos/main.asm
+++ asm_x86_macos/main.asm
@@ -1,23 +1,30 @@
 global start
 section .text
+
 start:
-  mov ecx, 100
+  mov rcx, 100
   top:
+  push rcx ; Push counter onto the stack
 
   ; Write to output
-  mov rax, 0x2000004 ; write
-  mov rdi, 1 ; stdout
-  mov rsi, msg
-  mov rdx, msg.len
-  syscall
+  ; mov rax, 0x2000004 ; write
+  ; mov rdi, 1 ; stdout
+  ; mov rsi, msg
+  ; mov rdx, msg.len
+  ; syscall
 
   ; GOTO top
+  pop rcx ; Pull counter off the stack
   loop top
+
+  mov rax, 12345
+  mov dword[rsp + 4], rax
+  callq _printf ; Call C function
 
   ; Exit
   mov rax, 0x2000001 ; exit
   mov rdi, 0
   syscall
 section .data
-msg:    db      "Hello, world!", 10
+msg: db "The number is %d",10,0
 .len:   equ     $ - msg
