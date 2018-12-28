1d1e777652228731c3358270123fab9ae69a4b21
William Johnston <william@johnstonhaus.us>
2018-12-27 19:23:07 -0600

    C#

---
+++ csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0) {
+                    Console.WriteLine("FizzBuzz");
+                } else if (i % 10 == 0) {
+                    Console.WriteLine("Buzz");
+                } else if (i % 5 == 0) {
+                    Console.WriteLine("Fizz");
+                } else {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
