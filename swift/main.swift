ab7a1cf2edfdd757362f58957c2ac69d2e6bca42
William Johnston <william@johnstonhaus.us>
2018-12-28 07:41:48 -0600

    Swift

---
+++ swift/main.swift
@@ -0,0 +1,11 @@
+for i in 1...100 {
+  if i % 15 == 0 {
+    print("FizzBuzz")
+  } else if i % 10 == 0 {
+    print("Buzz")
+  } else if i % 5 == 0 {
+    print("Fizz")
+  } else {
+    print(i)
+  }
+}
